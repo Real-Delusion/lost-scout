abb35ddb308bdcc9c34fe4c84be08e7b29cacbb8
Alex Mira <almiab1@epsg.upv.es>
2019-03-05 17:45:12 +0100

    script basico activar animación

---
+++ Assets/Scenes/MecanicaPalanca/mecanicaPalanca.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mecanicaPalanca : MonoBehaviour
+{
+    private Animator animacionPalanca; //Animacion de la placa de presion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacionPalanca = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("TRIGGER ENTER");
+            animacionPalanca.SetBool("OnOff", !animacionPalanca.GetBool("OnOff"));
+
+        }
+    }
+
+    //Cuando entre en el collider, animación = true
+    public void OnTriggerStay(Collider other)
+    {
+        
+    }
+    
+}
