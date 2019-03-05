519f1a108ae32eb3f1e0894c73d941498e6ddfde
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-03-05 16:44:39 +0100

    script basicos

---
+++ Assets/Scenes/SubirACaja/CajaParaSubir.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaParaSubir : MonoBehaviour
+{
+    // radio desde el cual se podrá interactuar
+    public float radio = 1.5f;
+
+    // player
+    private GameObject player;
+
+    // script empujarcaja del player
+    private SubirACaja subirScript;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radio);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // guardamos el player con el tag
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // accedemos a su script de empujarcaja
+        subirScript = player.GetComponent<SubirACaja>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si el jugador entra en el radio del objeto
+        if (Vector3.Distance(player.transform.position, transform.position) < radio)
+        {
+            Debug.Log("Pulse E para subirse a la caja");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                // cambiamos el estado del player a empujando
+                subirScript.Estado =SubirACaja.EstadosPlayer.Subir;
+            }
+            else
+            {
+                // mientras no pulse E
+                // mantenemos el estado del player en andando
+                subirScript.Estado = SubirACaja.EstadosPlayer.Andar;
+            }
+        }
+
+        // Si está fuera del radio del objeto
+        else
+        {
+            // cambiamos el estado del player a andando
+            subirScript.Estado = SubirACaja.EstadosPlayer.Andar;
+        }
+    }
+}
---
+++ Assets/Scenes/SubirACaja/SubirACaja.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubirACaja : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // -------------------------------- EstadosPlayer -----------------------------------
+    public enum EstadosPlayer
+    {
+        Andar,
+        Empujar,
+        Subir
+    }
+
+    private EstadosPlayer _estado = EstadosPlayer.Andar;
+
+    public EstadosPlayer Estado
+    {
+        get => _estado;
+        set
+        {
+            _estado = value;
+            if (_estado == EstadosPlayer.Empujar)
+            {
+                Debug.Log("empujando");
+                // aquí añadiremos animación de empujar del personaje
+            }
+
+            if (_estado == EstadosPlayer.Subir)
+            {
+                Debug.Log("Subiendo");
+                // aquí añadiremos animación de subir del personaje
+            }
+        }
+    }
+    // -------------------------------- EstadosPlayer -----------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { /*
+      // Si el jugador entra en el radio del objeto
+        if (Vector3.Distance(player.transform.position, transform.position) < radio)
+        {
+            Debug.Log("Mantén pulsado E para empujar la caja");
+            if (Input.GetKey(KeyCode.E))
+            {
+                Estado = EstadosCaja.Dinamico;
+
+                // cambiamos el estado del player a empujando
+                empujarScript.Estado = EmpujarCaja.EstadosPlayer.Empujar;
+            }
+            else
+            {
+                Estado = EstadosCaja.Estatico;
+            }
+        }
+
+        // Si está fuera del radio del objeto
+        else
+        {
+            Estado = EstadosCaja.Estatico;
+
+            // cambiamos el estado del player a andando
+            empujarScript.Estado = EmpujarCaja.EstadosPlayer.Andar;
+        }
+        */
+    }
+}
