a4b3ee5aaa8ebfd53fb57945fdfd09e8ad101c32
Santiago <sanpetor@epsg.upv.es>
2019-03-08 02:35:29 +0100

    Código piedra comentado

--- Assets/Scenes/MecanicaPiedra/LanzarPiedra.cs
+++ Assets/Scenes/MecanicaPiedra/LanzarPiedra.cs
@@ -4,31 +4,44 @@ using UnityEngine;
 
 public class LanzarPiedra : MonoBehaviour
 {
+    //Prefab piedra
     public GameObject objeto;
-    public float m_jumpY;
-    public float vel;
+    //Regular parábola en el eje Y
+    public float m_jumpY = 50f;
+    //Velocidad lanzamiento piedra
+    public float vel = 2f;
 
+    private bool piedra = false; //La piedra es false cuando no está en el escenario
+
+    //Rigidbody de la piedra
     private Rigidbody rbody;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rbody = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+       
+       if (Input.GetKeyDown(KeyCode.Mouse0) && !piedra) // Si se pulsa el boton izq. del mouse y la piedra es false
+       {
+                //Creo una nueva piedra
+                GameObject nuevaPiedra = Instantiate(objeto) as GameObject;
+                //Posicion donde empieza la piedra
+                nuevaPiedra.transform.position = transform.position + transform.forward * 1;
+                Rigidbody rb = nuevaPiedra.GetComponent<Rigidbody>();
+                //Añadir velocidad a la piedra
+                rb.velocity = transform.forward * vel;
+                //Añadir una fuerza en el eje Y
+                rb.AddForce(new Vector3(0, m_jumpY, 0));
+                piedra = true; //La piedra está en el escenario
+                
+        }
+        //Destruir piedra a los 5s
+        Destroy(GameObject.Find("Piedra(Clone)"), 5);
+
+        if (!GameObject.Find("Piedra(Clone)")) //Si no hay ningun objeto que se llame Piedra(Clone), puedes volver a lanzar la piedra
         {
-            GameObject piedra = Instantiate(objeto) as GameObject;
-            piedra.transform.position = transform.position + transform.forward * 1;
-            Rigidbody rb = piedra.GetComponent<Rigidbody>();
-            rb.velocity = transform.forward * vel;
-            rb.AddForce(new Vector3(0,m_jumpY,0));
+            piedra = false; //La piedra no está en el escenario
         }
 
-        Destroy(GameObject.Find("Piedra(Clone)"), 5);
     }
 
 }
