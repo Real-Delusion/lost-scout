2b6edba379735b82b0c2193e6cdb99e8ad98b80b
Santiago <sanpetor@epsg.upv.es>
2019-03-08 20:54:57 +0100

    Update placa presion

--- Assets/Scenes/PlacaDePresion/AnimPlacaPresion/PlacaDePresion.cs
+++ Assets/Scenes/PlacaDePresion/AnimPlacaPresion/PlacaDePresion.cs
@@ -7,10 +7,41 @@ public class PlacaDePresion : MonoBehaviour
     private Animator animacionPlaca; //Animacion de la placa de presion
     public Animator animacionPlataforma; //Hay que asignarle el animator de la plataforma que queremos que se mueva
 
+    public enum EstadosPlaca
+    {
+        On,
+
+        Off
+    }
+
+    private EstadosPlaca _estado = EstadosPlaca.Off;
+
+    public EstadosPlaca Estado
+    {
+        get => _estado;
+        set
+        {
+            _estado = value;
+            if (_estado == EstadosPlaca.On)
+            {
+                //Animación bajar la placa de presión y subir la plataforma
+                animacionPlaca.SetBool("bajarPlaca", !animacionPlaca.GetBool("bajarPlaca"));
+                animacionPlataforma.SetBool("subirPlataforma", !animacionPlataforma.GetBool("subirPlataforma"));
+            }
+
+            else if(_estado == EstadosPlaca.Off)
+            {
+                //Animación subir la placa de presión y bajar la plataforma
+                animacionPlaca.SetBool("bajarPlaca", !animacionPlaca.GetBool("bajarPlaca"));
+                animacionPlataforma.SetBool("subirPlataforma", !animacionPlataforma.GetBool("subirPlataforma"));
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Empieza");
+        //Debug.Log("Empieza");
         animacionPlaca = GetComponent<Animator>();
     }
 
@@ -23,17 +54,15 @@ public class PlacaDePresion : MonoBehaviour
     //Cuando entre en el collider, animación = true
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("TRIGGER ENTER");
-        //Animación bajar la placa de presión y subir la plataforma
-        animacionPlaca.SetBool("bajarPlaca", !animacionPlaca.GetBool("bajarPlaca"));
-        animacionPlataforma.SetBool("subirPlataforma",!animacionPlataforma.GetBool("subirPlataforma"));
+        //Debug.Log("TRIGGER ENTER");
+        Estado = EstadosPlaca.On;
+
     }
     //Cuando salga del collider,animación = false
     public void OnTriggerExit(Collider other)
     {
-        Debug.Log("TRIGGER EXIT");
-        //Animación subir la placa de presión y bajar la plataforma
-        animacionPlaca.SetBool("bajarPlaca", !animacionPlaca.GetBool("bajarPlaca"));
-        animacionPlataforma.SetBool("subirPlataforma",!animacionPlataforma.GetBool("subirPlataforma"));
+        //Debug.Log("TRIGGER EXIT");
+        Estado = EstadosPlaca.Off;
+
     }
 }
