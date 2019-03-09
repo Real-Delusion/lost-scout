fb56b339618431f310b2c8fb70d8b7f0a01541cd
Alex Mira <almiab1@epsg.upv.es>
2019-03-09 14:05:56 +0100

    correccion errores mecanica palanca + comentado codigo script

--- Assets/Scenes/MecanicaPalanca/mecanicaPalanca.cs
+++ Assets/Scenes/MecanicaPalanca/mecanicaPalanca.cs
@@ -4,35 +4,43 @@ using UnityEngine;
 
 public class mecanicaPalanca : MonoBehaviour
 {
-    private Animator animacionPalanca;
-    public Animator animPared; 
-    public float radio = 8f;
+    // animacion de la palanca - estado publico, para que se pueda utilizar desde cualquier scene
+    public Animator animacionPalanca;
+    // animacion del objeto que active la palanca - estado publico, para que se pueda utilizar desde cualquier scene
+    public Animator animObjeto; 
+    // radio del area en la que se podrá activar la palanca - estado publico (se puede modificar)
+    public float radio = 2f;
+    // referencia al player(personaje)
     public Transform player;
 
     // Maquinas de estados finitos
     public enum EstadosPalanca
     {
-        On,
-        Off,
+        On, // estado activo
+        Off, // estado apagado
     }
 
-    private EstadosPalanca _estadoP = EstadosPalanca.Off;
+    private EstadosPalanca _estadoP = EstadosPalanca.Off; // estado de la palanca por defecto --> desactiva
 
+    // especificación de que hara la palanca dependiendo en el estado en el que este ON / OFF
     public EstadosPalanca Estado
     {
         get => _estadoP;
         set
         {
             _estadoP = value;
-            if (_estadoP == EstadosPalanca.On)
+            // caso en que la palanca este ON/Activa
+            if (_estadoP == EstadosPalanca.On) 
             {
-                animacionPalanca.SetBool("OnOff", true);
-                animPared.SetBool("UpDown", true);
+                animacionPalanca.SetBool("OnOff", true); // Se ejecuta la animación para que se ponga la palanca en posicion activa
+                animObjeto.SetBool("UpDown", true); // Se ejecuta la animación del objeto que activa
             }
-            if (_estadoP == EstadosPalanca.Off)
+
+            // caso en que la palanca este ON/Activa
+            if (_estadoP == EstadosPalanca.Off) 
             {
-                animacionPalanca.SetBool("OnOff", false);
-                animPared.SetBool("UpDown", false);
+                animacionPalanca.SetBool("OnOff", false); // Se ejecuta la animación para que se ponga la palanca en posicion desactiva
+                animObjeto.SetBool("UpDown", false); // Se ejecuta la animacion del objeto para que vuelva a su lugar inicial
             }
         }
     }
@@ -41,31 +49,34 @@ public class mecanicaPalanca : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animacionPalanca = GetComponent<Animator>();
+        //animacionPalanca = GetComponent<Animator>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        // comprobacion de los estados de la palanca
         switch (Estado)
         {
-            case EstadosPalanca.Off:
-                if (Vector3.Distance(transform.position, player.position) < radio)
+            //  caso en que la palanca este desactiva OFF
+            case EstadosPalanca.Off: 
+                if (Vector3.Distance(transform.position, player.position) < radio) // si el player esta dentro del area de accion 
                 {
-                    if (Input.GetKey(KeyCode.E))
+                    if (Input.GetKeyDown(KeyCode.E)) // si se pulsa la tecla "E" destro del radio
                     {
-                        Estado = EstadosPalanca.On;
+                        Estado = EstadosPalanca.On; // Se cambia la panca a estado ON / Activo
                     }
                 }
                 break;
 
+            //  caso en que la palanca este activa ON 
             case EstadosPalanca.On:
-                if (Vector3.Distance(transform.position, player.position) < radio)
+                if (Vector3.Distance(transform.position, player.position) < radio) // si el player esta dentro del area de accion 
                 {
-                    if (Input.GetKey(KeyCode.E))
+                    if (Input.GetKeyDown(KeyCode.E)) // si se pulsa la tecla "E" destro del radio
                     {
-                        Estado = EstadosPalanca.Off;
+                        Estado = EstadosPalanca.Off; // Se cambia la panca a estado OFF / Desactiva
                     }
                 }
                 break;
