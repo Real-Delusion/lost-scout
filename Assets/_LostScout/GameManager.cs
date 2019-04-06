8f26548578955d8cd2272ff0f92c5d0df28dc4cd
Joan Ciprià <joancipria@gmail.com>
2019-04-06 18:27:13 +0200

    Incorporada la creación de niveles

--- Assets/_LostScout/GameManager.cs
+++ Assets/_LostScout/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public List<Nivel> niveles;
+    
     GameObject player;
     GameObject checkpoint;
 
@@ -14,27 +16,43 @@ public class GameManager : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        checkpoint = GameObject.FindGameObjectWithTag("checkpoint");
-        Gizmos.DrawWireSphere(checkpoint.transform.position, radioCheckpoint);
+        //checkpoint = GameObject.FindGameObjectWithTag("checkpoint");
+        //Gizmos.DrawWireSphere(checkpoint.transform.position, radioCheckpoint);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene ();
+
+        // Create Levels
+        niveles =  new List<Nivel>();
+
+        var nivel1 = new Nivel(1,"Nivel 1",false,0,false,60,1);
+        var nivel2 = new Nivel(2,"Nivel 2",false,0,false,60,5);
+        var nivel3 = new Nivel(3,"Nivel 3",false,0,true,60,5);
+        
+        niveles.Add(nivel1);
+        niveles.Add(nivel2);
+        niveles.Add(nivel3);
+
         Cursor.visible = false;
-        player = GameObject.FindGameObjectWithTag("Player");
+        /*player = GameObject.FindGameObjectWithTag("Player");
         checkpoint = GameObject.FindGameObjectWithTag("checkpoint");
+        */
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {/*
         Debug.Log(Vector3.Distance(player.transform.position, checkpoint.transform.position));
         if (Vector3.Distance(player.transform.position, checkpoint.transform.position) < radioCheckpoint)
         {
             // aquí incluir animación del player
             SceneManager.LoadScene("SeleccionNivel");
         }
+         */
     }
 
 }
