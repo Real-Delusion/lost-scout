7c807b42960f70adc04da8e84419b16f79c69815
Joan Ciprià <joancipria@gmail.com>
2019-04-07 20:28:47 +0200

    Solucionados algunos bugs y reestablecido el checkpoint

--- Assets/_LostScout/GameManager.cs
+++ Assets/_LostScout/GameManager.cs
@@ -7,14 +7,15 @@ public class GameManager : MonoBehaviour
 {
     //Static instance of GameManager which allows it to be accessed by any other script.
     public static GameManager instance = null;
-    public List<Nivel> niveles;
+    Scene currentScene;
+    public static List<Nivel> niveles;
     public NivelesManager nivelesManager;
     
     GameObject player;
     GameObject checkpoint;
 
     // radio desde el cual se podrá cambiar de escena tocando el checkpoint
-    public float radioCheckpoint = 1.0f;
+    public static float radioCheckpoint = 1.5f;
 
     private void OnDrawGizmos()
     {
@@ -24,12 +25,13 @@ public class GameManager : MonoBehaviour
     }
 
     //Awake is always called before any Start functions
+    // called zero
     void Awake()
     {
         //Check if instance already exists
         if(instance == null){
             instance = this; //if not, set instance to this
-            InitGame(); //Call the InitGame function to initialize the game
+            
         }else if (instance != this){ //If instance already exists and it's not this:
             Destroy(gameObject); //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
         }
@@ -39,11 +41,24 @@ public class GameManager : MonoBehaviour
         
         // Hide cursor
         Cursor.visible = false;
-
+        
         nivelesManager = GetComponent<NivelesManager>();
+        InitGame(); //Call the InitGame function to initialize the game
+
 
         // Create a temporary reference to the current scene.
-        Scene currentScene = SceneManager.GetActiveScene ();
+        currentScene = SceneManager.GetActiveScene ();
+    }
+    // called first
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = SceneManager.GetActiveScene ();
 
         // Enable cursor and print levels if we are in SeleccionNivel scene
         if(currentScene.name == "SeleccionNivel"){
@@ -56,35 +71,45 @@ public class GameManager : MonoBehaviour
             Cursor.visible = true;
         }
 
+        if(currentScene.name == "Nivel 1"){
+            player = GameObject.FindGameObjectWithTag("Player");
+            checkpoint = GameObject.FindGameObjectWithTag("checkpoint");
+        }
+    }
 
-        /*player = GameObject.FindGameObjectWithTag("Player");
-        checkpoint = GameObject.FindGameObjectWithTag("checkpoint");
-        */
+    // called third
+    void Start()
+    {
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 
     void InitGame(){
         // Create Levels
-        niveles =  new List<Nivel>();
-
-        var nivel1 = new Nivel(1,"Nivel 1",false,0,false,60,1);
-        var nivel2 = new Nivel(2,"Nivel 2",false,0,false,60,5);
-        var nivel3 = new Nivel(3,"Nivel 3",false,0,true,60,5);
-        
-        niveles.Add(nivel1);
-        niveles.Add(nivel2);
-        niveles.Add(nivel3);
+        niveles =  new List<Nivel>()
+        {
+            new Nivel(1,"Nivel 1",false,0,false,60,1),
+            new Nivel(2,"Nivel 2",false,0,false,60,5),
+            new Nivel(3,"Nivel 3",false,0,true,60,5)
+        };
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        Debug.Log(Vector3.Distance(player.transform.position, checkpoint.transform.position));
-        if (Vector3.Distance(player.transform.position, checkpoint.transform.position) < radioCheckpoint)
-        {
+    {
+        if (currentScene.name == ("Nivel 1")){
+            //Debug.Log(Vector3.Distance(player.transform.position, checkpoint.transform.position)+"   "+radioCheckpoint);
+            if (Vector3.Distance(player.transform.position, checkpoint.transform.position) < radioCheckpoint)
+            {
             // aquí incluir animación del player
             SceneManager.LoadScene("SeleccionNivel");
+            }
         }
-         */
+         
     }
 
 }
