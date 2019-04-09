94ef69cc73612d79784872b37fc1b7903b48f36a
Joan Ciprià <joancipria@gmail.com>
2019-04-10 01:35:06 +0200

    Finalizado sistema chapuzas de punuación

--- Assets/_LostScout/Scripts/GameManager/UIManager.cs
+++ Assets/_LostScout/Scripts/GameManager/UIManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class UIManager : MonoBehaviour
 {
@@ -8,6 +10,8 @@ public class UIManager : MonoBehaviour
     public GameObject menuPuntuacion;
     public GameObject insigniaHabilidadGrey;
     public GameObject insigniaPrestigioGrey;
+    public Text tiempo;
+    public Text tiempoRecord;
 
     // Start is called before the first frame update
     void Start()
@@ -27,11 +31,17 @@ public class UIManager : MonoBehaviour
 
     public void showMenuPuntuacion (int insignias, float time){
         menuPuntuacion.SetActive(true);
-        if(insignias == 2){
+        Debug.Log(insignias);
+        tiempo.text = (Math.Round(time,2).ToString())+" s";
+        if(insignias >= 2){
             insigniaHabilidadGrey.SetActive(false);
+        }else{
+            insigniaHabilidadGrey.SetActive(true); 
         }
-        if(insignias == 3){
+        if(insignias >= 3){
             insigniaPrestigioGrey.SetActive(false);
+        }else{
+            insigniaPrestigioGrey.SetActive(true);
         }
     }
 
