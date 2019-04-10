4f359733c5f02c8fb1db63fcc2da6b6b06b8c26d
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-04-10 02:00:59 +0200

    menu screen niveles flotando

---
+++ Assets/_LostScout/Scripts/RotateNivel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateNivel : MonoBehaviour
+{    
+    public float velocidad = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+        void Update(){
+        transform.Rotate(0, velocidad * Time.deltaTime, 0);
+    }
+}
