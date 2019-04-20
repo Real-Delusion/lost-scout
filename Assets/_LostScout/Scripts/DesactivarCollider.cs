67c5a12903a0fea029ec36512129338b7bb75ebb
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-04-20 19:48:33 +0200

    script desactivarcollider y placa presion implementado

---
+++ Assets/_LostScout/Scripts/DesactivarCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Este script desactiva un collider cualquiera a partir de la posición del hijo de un objeto,
+// así podemos abrir/cerrar caminos dependiendo de dónde se encuentre un objeto (como un rio, 
+// un puente, una plataforma etc)
+public class DesactivarCollider : MonoBehaviour
+{
+    // La posición la escribimos en Unity
+    public float xPosicion;
+    public float yPosicion;
+    public float zPosicion;
+
+    // El collider que queremos desactivar, asignamos en Unity
+    public Collider colliderObjeto;
+    private Vector3 posicion;
+
+    void Start()
+    {
+        // La posición a partir de la cual desactivamos el collider
+        posicion = new Vector3(xPosicion, yPosicion, zPosicion);
+
+        //Debug.Log(posicion);
+        //Debug.Log(colliderObjeto);
+    }
+
+    void Update()
+    {
+        //Debug.Log(transform.GetChild(0).position);
+        if (transform.GetChild(0).position == posicion) {
+            colliderObjeto.enabled = false;
+        }
+        else {
+            colliderObjeto.enabled = true;
+        }
+    }
+}
