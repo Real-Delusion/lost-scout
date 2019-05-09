18bd1d28a78519eac7b9788d3b9c82b1c39bb010
Joan Ciprià <joancipria@gmail.com>
2019-05-09 19:53:01 +0200

    Recuperado script subir caja

---
+++ Assets/_LostScout/Scripts/CajaParaSubir.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Este script se asigna a un gameobject al que el player se podrá subir
+
+public class CajaParaSubir : MonoBehaviour
+{
+    // radio desde el cual se podrá interactuar
+    public float radio = 1.5f;
+
+    // player
+    private GameObject player;
+
+    // script playercontroller del player
+    private PlayerController playerController;
+
+    // altura del game object, para luego asignar la nueva posición al player
+    private float miAltura;
+
+
+    // para ver el radio en la escena
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, radio);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // guardamos el player con el tag
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // accedemos a su script de playercontroller
+        playerController = player.GetComponent<PlayerController>();
+
+        // accedemos a la altura del game object (eje y)
+        miAltura = GetComponent<Collider>().bounds.size.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si el jugador entra en el radio del objeto
+        if (Vector3.Distance(player.transform.position, transform.position) < radio)
+        {
+            Debug.Log("Pulse espacio para subirse a la caja");
+            // Si está en el radio y pulsa espacio
+            if (Input.GetKeyDown(KeyCode.Space) | Input.GetKeyDown("joystick button 0"))
+            {
+                // mandamos la posición y la altura de la caja al otro script
+                // el script playercontroller cambiará la posición del player usando estos parámetros
+                playerController.posicionTronco = transform.position;
+                playerController.alturaTronco = miAltura;
+
+                // cambiamos el estado del player a subir
+                playerController.Estado = PlayerController.EstadosPlayer.Subir;
+            }
+
+        }
+
+        
+    }
+}
