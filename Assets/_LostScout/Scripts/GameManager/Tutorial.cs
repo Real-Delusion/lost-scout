791352cdfc3f46b55228833fc527ef7f6aef226c
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-05-25 15:10:22 +0200

    animar mensajes tutorial

--- Assets/_LostScout/Scripts/GameManager/Tutorial.cs
+++ Assets/_LostScout/Scripts/GameManager/Tutorial.cs
@@ -10,11 +10,25 @@ public class Tutorial : MonoBehaviour
     public GameObject segundaPalanca;
     public GameObject segundoTronco;
     private float startMousePos;
+
+    public GameObject camaraText;
+    public GameObject moveText;
+    public GameObject leverText;
+    public GameObject climbtext;
+    public GameObject pickUpText;
     GameObject player;
+    bool pressedE = false;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        camaraText =  textosTutorial.gameObject.transform.GetChild(0).gameObject;
+        moveText = textosTutorial.gameObject.transform.GetChild(1).gameObject;
+        leverText = textosTutorial.gameObject.transform.GetChild(2).gameObject;
+        climbtext = textosTutorial.gameObject.transform.GetChild(3).gameObject;
+        pickUpText = textosTutorial.gameObject.transform.GetChild(4).gameObject;
+
         showMouse();
         startMousePos = Input.mousePosition.x;
     }
@@ -22,95 +36,114 @@ public class Tutorial : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (textosTutorial.gameObject.transform.GetChild(0).gameObject.active && startMousePos != Input.mousePosition.x) {
+        // ROTAR CAMARA
+        if (camaraText.activeSelf && startMousePos != Input.mousePosition.x) {
             StartCoroutine(Wait("move"));
         }
-        if (primeraPalanca.GetComponent<mecanicaPalanca>().inRange || segundaPalanca.GetComponent<mecanicaPalanca>().inRange) {
-            showLever();
+
+        // ACCIONAR PALANCA
+        if (primeraPalanca.GetComponent<mecanicaPalanca>().inRange && pressedE == false) {
+            StartCoroutine(Wait("lever"));
         }
-        else {
-            hideLever();
-        }        
+        else if (!primeraPalanca.GetComponent<mecanicaPalanca>().inRange) {
+            StartCoroutine(Wait("hidelever"));
+        }
+
+        // SUBIR TRONCO  
         if (primerTronco.GetComponent<tronco>().enRadio) {
-            showClimb();
+            StartCoroutine(Wait("climb"));
         } 
         else {
             hideClimb();
         }
-        if (segundoTronco.GetComponent<tronco>().enRadio) {
-            showPickUp();
+
+        // COGER TRONCO
+        if (segundoTronco.GetComponent<tronco>().enRadio && !segundoTronco.GetComponent<tronco>().carrying) {
+            StartCoroutine(Wait("pickup"));
         }
         else {
             hidePickUp();
         }
+        
+        if (Input.GetKeyDown(KeyCode.E)) pressedE = true;
     }
 
     public void showMouse() {
-        textosTutorial.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        textosTutorial.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        camaraText.SetActive(true);
+        camaraText.GetComponent<Animator>().SetBool("show", true);
+        moveText.SetActive(false);
+        climbtext.SetActive(false);
+        pickUpText.SetActive(false);
     }
 
     public void showMovement() {
-        textosTutorial.gameObject.transform.GetChild(1).gameObject.SetActive(true);
-        textosTutorial.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        camaraText.SetActive(false);
+        moveText.SetActive(true);
+        moveText.GetComponent<Animator>().SetBool("show", true);
     }
 
     public void showLever() {
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(true);
-        textosTutorial.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(false);
-    }
-
-    
-    public void showPickUp() {
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(true);
-        textosTutorial.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        moveText.SetActive(false);
+        leverText.SetActive(true);
+        leverText.GetComponent<Animator>().SetBool("show", true);
     }
 
     public void showClimb() {
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(true);
-        textosTutorial.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(0).gameObject.SetActive(false);
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        climbtext.SetActive(true);
+        climbtext.GetComponent<Animator>().SetBool("show", true);
+    }
+
+    public void showPickUp() {
+        pickUpText.SetActive(true);
+        pickUpText.GetComponent<Animator>().SetBool("show", true);
+    }
+
+    public void hideMouse() {
+        camaraText.GetComponent<Animator>().SetBool("show", false);
+    }
+
+    public void hideMovement() {
+        if (moveText.GetComponent<Animator>().GetBool("show"))
+        moveText.GetComponent<Animator>().SetBool("show", false);
     }
  
-    public void hideClimb() {
-        textosTutorial.gameObject.transform.GetChild(3).gameObject.SetActive(false);
-    }
-        
     public void hideLever() {
-        textosTutorial.gameObject.transform.GetChild(2).gameObject.SetActive(false);
+        leverText.GetComponent<Animator>().SetBool("show", false);
+    }
+    public void hideClimb() {
+        climbtext.GetComponent<Animator>().SetBool("show", false);
     }
 
     public void hidePickUp() {
-        textosTutorial.gameObject.transform.GetChild(4).gameObject.SetActive(false);
+        pickUpText.GetComponent<Animator>().SetBool("show", false);
     }
 
     IEnumerator Wait(string method)
     {
-        yield return new WaitForSecondsRealtime(5);
         switch (method) {
           case "move":
-              showMovement();
-              break;
-          case "climb":
-              showClimb();
-              break;
+                yield return new WaitForSecondsRealtime(5);
+                hideMouse();
+                yield return new WaitForSecondsRealtime(1);
+                showMovement();
+                break;
           case "lever":
-              showLever();
-              break;
+                hideMovement();
+                yield return new WaitForSecondsRealtime(1);
+                showLever();
+                break;
+          case "hidelever":
+                hideLever();
+                yield return new WaitForSecondsRealtime(1);
+                break;
+          case "climb":
+                yield return new WaitForSecondsRealtime(1);
+                showClimb();
+                break;
+          case "pickup":
+                yield return new WaitForSecondsRealtime(1);
+                showPickUp();
+                break;
         }
     }
 
