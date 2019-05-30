afd2db639e9ae1359c08df4c2db545027326acb9
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-05-31 00:06:16 +0200

    casi tamoooo

--- Assets/_LostScout/Scenes/Levels/Level X/LevelScript.cs
+++ Assets/_LostScout/Scenes/Levels/Level X/LevelScript.cs
@@ -7,6 +7,7 @@ public class LevelScript : MonoBehaviour
     //Palancas y placas de presion
     public GameObject paloPalancaRoca;
     public GameObject paloPalancaCueva;
+    public GameObject troncoOculto;
 
     public GameObject placaPresionIzq;
     public GameObject placaPresionDcha;
@@ -15,11 +16,12 @@ public class LevelScript : MonoBehaviour
     public Animator animatorRoca;
     public Animator animatorPuerta;
     public Animator animatorTronco;
+    private GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -29,7 +31,6 @@ public class LevelScript : MonoBehaviour
         string estadoPalancaRoca = paloPalancaRoca.GetComponent<mecanicaPalanca>().Estado.ToString();
         string estadoPlacaIzq = placaPresionIzq.GetComponent<PlacaDePresion>().Estado.ToString();
         string estadoPlacaDcha = placaPresionDcha.GetComponent<PlacaDePresion>().Estado.ToString();
-
 
         // PALANCA CUEVA
         if (estadoPalancaCueva.Equals("On"))
@@ -47,7 +48,7 @@ public class LevelScript : MonoBehaviour
             animatorTronco.SetBool("down", false);          
         }
         
-        // PLACA IZQUIERDA
+        // PLACA UNA U OTRA
         if (estadoPlacaIzq.Equals("On") || estadoPlacaDcha.Equals("On")) {
             animatorPuerta.SetFloat("nivelApertura", 1);
         }
@@ -55,9 +56,16 @@ public class LevelScript : MonoBehaviour
             animatorPuerta.SetFloat("nivelApertura", 0);
         }
 
-                // PLACA IZQUIERDA
+        // PLACAS AMBAS
         if (estadoPlacaIzq.Equals("On") && estadoPlacaDcha.Equals("On")) {
             animatorPuerta.SetFloat("nivelApertura", 2);
         }
+
+        // PLAYER
+        if (troncoOculto.GetComponent<tronco>().Estado == tronco.EstadosCaja.Estatico) {
+            GameObject troncoPadre = GameObject.FindGameObjectWithTag("troncoPadre");
+            troncoPadre.transform.position = troncoOculto.transform.position;
+            troncoOculto.transform.SetParent(troncoPadre.transform);
+        } 
     }
 }
