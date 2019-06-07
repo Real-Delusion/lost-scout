ecc544e8880c8aba327af671e8d143761360cd3b
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-06-07 13:57:43 +0200

    arreglado level 4  se suponeeeee

--- Assets/_LostScout/Scenes/Levels/Level 4/Level4.cs
+++ Assets/_LostScout/Scenes/Levels/Level 4/Level4.cs
@@ -7,14 +7,13 @@ public class Level4 : MonoBehaviour
     //Palancas y placas de presion
     public GameObject paloPalancaRoca;
     public GameObject paloPalancaCueva;
-    public GameObject troncoOculto;
     public GameObject placaPresionIzq;
     public GameObject placaPresionDcha;
 
     //Animators
     public Animator animatorRoca;
     public Animator animatorPuerta;
-    public Animator animatorTronco;
+    public Animator animatorPlaca;
     private GameObject player;
 
     // Start is called before the first frame update
@@ -40,11 +39,11 @@ public class Level4 : MonoBehaviour
         // PALANCA ROCA
         if (estadoPalancaRoca.Equals("On")) {
             animatorRoca.SetBool("UpDown", true);
-            animatorTronco.SetBool("down", true);
+            animatorPlaca.SetBool("Hide", true);
         }
         else {
-            animatorRoca.SetBool("UpDown", false);
-            animatorTronco.SetBool("down", false);          
+           animatorRoca.SetBool("UpDown", false);
+           animatorPlaca.SetBool("Hide", false);          
         }
         
         // PLACA UNA U OTRA
@@ -59,21 +58,5 @@ public class Level4 : MonoBehaviour
         if (estadoPlacaIzq.Equals("On") && estadoPlacaDcha.Equals("On")) {
             animatorPuerta.SetFloat("nivelApertura", 2);
         }
-
-        // PLAYER
-        if (troncoOculto.GetComponent<tronco>().cayendo == true) {
-            GameObject troncoPadre = GameObject.FindGameObjectWithTag("troncoPadre");
-            troncoPadre.transform.position = troncoOculto.transform.position;
-            troncoOculto.transform.SetParent(troncoPadre.transform);
-            StartCoroutine(Wait());
-            Debug.Log(troncoOculto.transform.position);
-        }
-    }
-
-    IEnumerator Wait()
-    {
-        GameObject troncoPadre = GameObject.FindGameObjectWithTag("troncoPadre");
-        yield return new WaitForSeconds(0.2f);
-        troncoPadre.transform.position = new Vector3(troncoPadre.transform.position.x, 0.0000f, troncoPadre.transform.position.z);
     }
 }
