0b9fb40a47ea2778b49cefa47b71de217248fee4
santipt <santiperez1199@gmail.com>
2019-06-09 14:35:29 +0200

    update level 6

--- Assets/_LostScout/Scenes/Levels/Level6/Level6.cs
+++ Assets/_LostScout/Scenes/Levels/Level6/Level6.cs
@@ -16,9 +16,11 @@ public class Level6 : MonoBehaviour
     //ANIMATORS
     public Animator animatorPuertaA1; //Grupo troncos 1
     public Animator animatorPuertaA2; //Puerta cueva 1
-    //public Animator animatorPuertaB1; //Grupo troncos 2
+    public Animator animatorPuertaB1; //Grupo troncos 2
     public Animator animatorPuertaB2; //Puerta cueva 2
-    public Animator animatorPlacaPresion1;
+    public Animator animatorPlacaPresion1; //Placa presion 1
+    public Animator animatorPlacaPresion2; //Placa presion 2
+
 
 
     // Start is called before the first frame update
@@ -37,15 +39,20 @@ public class Level6 : MonoBehaviour
         string estadoPresion1 = placaPresion1.GetComponent<PlacaDePresion>().Estado.ToString();
         string estadoPresion2 = placaPresion2.GetComponent<PlacaDePresion>().Estado.ToString();
 
+        //--------------- PARTE 1 -------------------
+
         //PALANCA 1
         if (estadoPalanca1.Equals("On"))
         {
             animatorPuertaA1.SetBool("open", true);
             animatorPuertaA2.SetBool("open", true);
 
+            //PALANCA 2
             if (estadoPalanca2.Equals("Off"))
             {
                 animatorPuertaB2.SetBool("open", false);
+
+                //PLACA PRESION 1
                 if (estadoPresion1.Equals("Off"))
                 {
                     animatorPlacaPresion1.SetBool("show", false);
@@ -58,6 +65,7 @@ public class Level6 : MonoBehaviour
             animatorPuertaA1.SetBool("open", false);
             animatorPuertaA2.SetBool("open", false);
         }
+
         //PALANCA 2
         if (estadoPalanca2.Equals("On"))
         {
@@ -65,22 +73,72 @@ public class Level6 : MonoBehaviour
             animatorPuertaB2.SetBool("open", true);
             animatorPlacaPresion1.SetBool("show", true);
 
+            //PALANCA 1
             if (estadoPalanca1.Equals("Off"))
             {
                 animatorPuertaA1.SetBool("open", false);
                 animatorPuertaA2.SetBool("open", false);
             }
         }
+
         //PLACA PRESION 1
         if (estadoPresion1.Equals("On"))
         {
             animatorPuertaB2.SetBool("open", true);
 
+            //PALANCA 2
             if (estadoPalanca2.Equals("Off"))
             {
                 animatorPuertaA2.SetBool("open", true);
+
+                //PALANCA 3
+                if (estadoPalanca3.Equals("Off"))
+                {
+                    animatorPuertaA1.SetBool("open", true);
+                    animatorPuertaB1.SetBool("open", false);
+                }
             }
         }
+
+        //--------------- PARTE 2 -------------------
+        //PALANCA 3
+        if (estadoPalanca3.Equals("On"))
+        {
+            animatorPuertaA1.SetBool("open", false);
+            animatorPuertaB1.SetBool("open", true);
+            animatorPlacaPresion2.SetBool("show", true);
+
+
+            //PALANCA 1
+            if (estadoPalanca1.Equals("Off"))
+            {
+                animatorPuertaA1.SetBool("open", false);
+                animatorPuertaA2.SetBool("open", false);
+            }
+
+            //PALANCA 2
+            if (estadoPalanca2.Equals("Off"))
+            {
+                animatorPuertaB2.SetBool("open", false);
+            }
+        }
+
+        //PLACA PRESION 2
+        if (estadoPresion2.Equals("On"))
+        {
+            if (estadoPalanca3.Equals("Off"))
+            {
+                animatorPuertaB1.SetBool("open", true);
+            }
+        }
+
+
+
+
+
+
+
+
 
 
     }
