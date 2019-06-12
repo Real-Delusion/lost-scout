7bc29055cffa36f2228f3949f5484d70ab8a41fc
Alex Mira <almiab1@epsg.upv.es>
2019-06-12 23:15:10 +0200

    terminado backend opts

--- Assets/_LostScout/Scripts/SettingMenu.cs
+++ Assets/_LostScout/Scripts/SettingMenu.cs
@@ -9,12 +9,49 @@ public class SettingMenu : MonoBehaviour
     public AudioMixer mixer;
     public Text txtMasterVolume;
     public Text txtMusicVolume;
+    public Dropdown resolutionDropDown;
+
+    Resolution[] resolutions;
 
     private void Start()
     {
         int final = (40 * 100) / 60;
         txtMasterVolume.text = final + "%";
         txtMusicVolume.text = final + "%";
+
+        //Graphics
+        QualitySettings.SetQualityLevel(QualitySettings.GetQualityLevel());
+
+        //Resolutions
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void SetMasterVolume (float volume)
@@ -31,5 +68,15 @@ public class SettingMenu : MonoBehaviour
         int primitivo = ((int)volume + 40);
         int final = (primitivo * 100) / 60;
         txtMusicVolume.text = final + "%";
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
