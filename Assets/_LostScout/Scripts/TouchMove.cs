5f9641c4d2de070e0f4c4f444c99b363c7f71006
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-06-18 13:19:05 +0200

    movimiento hacia donde clicas

--- Assets/_LostScout/Scripts/TouchMove.cs
+++ Assets/_LostScout/Scripts/TouchMove.cs
@@ -2,70 +2,68 @@
 using System.Collections;
 using UnityEngine.iOS;
 
+
 // Attach to an origin based cube.
 // A screen touch moves the cube on an iPhone or iPad.
 // A second screen touch reduces the cube size.
 
 public class TouchMove : MonoBehaviour
 {
-    private Vector3 position;
-    private float width;
-    private float height;
-
-    void Awake()
-    {
-        width = (float)Screen.width / 2.0f;
-        height = (float)Screen.height / 2.0f;
-
-        // Position used for the cube.
-        position = new Vector3(0.0f, 0.0f, 0.0f);
+//flag to check if the user has tapped / clicked. 
+ //Set to true on click. Reset to false on reaching destination
+ private bool flag = false;
+ //destination point
+ private Vector3 endPoint;
+ //alter this to change the speed of the movement of player / gameobject
+ public float duration = 50.0f;
+ //vertical position of the gameobject
+ private float yAxis;
+ 
+ void Start(){
+  //save the y axis value of gameobject
+  yAxis = gameObject.transform.position.y;
+ }
+  
+ // Update is called once per frame
+ void Update () {
+ 
+  //check if the screen is touched / clicked   
+  if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || (Input.GetMouseButtonDown(0)))
+  {
+   //declare a variable of RaycastHit struct
+   RaycastHit hit;
+   //Create a Ray on the tapped / clicked position
+   Ray ray;
+   //for unity editor
+   #if UNITY_EDITOR
+   ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+   //for touch device
+   #elif (UNITY_ANDROID || UNITY_IPHONE || UNITY_WP8)
+   ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+   #endif
+ 
+   //Check if the ray hits any collider
+   if(Physics.Raycast(ray,out hit))
+   {
+    //set a flag to indicate to move the gameobject
+    flag = true;
+    //save the click / tap position
+    endPoint = hit.point;
+    //as we do not want to change the y axis value based on touch position, reset it to original y axis value
+    endPoint.y = yAxis;
+    Debug.Log(endPoint);
+   }
+    
+  }
+  //check if the flag for movement is true and the current gameobject position is not same as the clicked / tapped position
+  if(flag && !Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude)){ //&& !(V3Equal(transform.position, endPoint))){
+   //move the gameobject to the desired position
+   gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, endPoint, 1/(duration*(Vector3.Distance(gameObject.transform.position, endPoint))));
+  }
+  //set the movement indicator flag to false if the endPoint and current gameobject position are equal
+  else if(flag && Mathf.Approximately(gameObject.transform.position.magnitude, endPoint.magnitude)) {
+   flag = false;
+   Debug.Log("I am here");
     }
-
-    void OnGUI()
-    {
-        // Compute a fontSize based on the size of the screen width.
-        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
-
-        GUI.Label(new Rect(20, 20, width, height * 0.25f),
-            "x = " + position.x.ToString("f2") +
-            ", y = " + position.y.ToString("f2"));
-    }
-
-    void Update()
-    {
-        // Handle screen touches.
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-
-            // Move the cube if the screen has the finger moving.
-            if (touch.phase == TouchPhase.Moved)
-            {
-                Vector2 pos = touch.position;
-                pos.x = (pos.x - width) / width;
-                pos.y = (pos.y - height) / height;
-                position = new Vector3(-pos.x, pos.y, 0.0f);
-
-                // Position the cube.
-                transform.position = position;
-            }
-
-            if (Input.touchCount == 2)
-            {
-                touch = Input.GetTouch(1);
-
-                if (touch.phase == TouchPhase.Began)
-                {
-                    // Halve the size of the cube.
-                    transform.localScale = new Vector3(0.75f, 0.75f, 0.75f);
-                }
-
-                if (touch.phase == TouchPhase.Ended)
-                {
-                    // Restore the regular size of the cube.
-                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-                }
-            }
-        }
-    }
+}
 }
